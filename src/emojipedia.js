6eebd9e9a9c98a61bfde68f4a0a8fe9a1a8b3129
souraOP <sourasishmondal95@gmail.com>
2023-07-15 12:07:09 +0530

    Adding files

---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "Gandu Biceps"
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Maaf Karde Bhai",
+    meaning:
+      "Sorry Bhai Galti ho gayi"
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "HAHAH",
+    meaning:
+      "Gand mein nhi hai guda toh maidan kai ko kuda"
+  }
+];
+
+export default emojipedia;
